51c3c9db0f3a75f4613126f44f95188ff44ce742
DevPablo <anenub4@gmail.com>
2023-07-26 12:05:24 +0300

    Basic main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int startScene = 1;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(startScene);
+    }
+
+    public void QuitGame() 
+    {
+        #if UNITY_EDITOR
+                // Application.Quit() does not work in the editor so
+                // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit();
+        #endif
+    }
+}
