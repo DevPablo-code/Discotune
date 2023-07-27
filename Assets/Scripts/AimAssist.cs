62ef06c5b7f598f3f52b920081988be68d6cd347
DevPablo <anenub4@gmail.com>
2023-07-27 16:41:12 +0300

    Add aim assist using crosshair delay

---
+++ Assets/Scripts/AimAssist.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AimAssist : MonoBehaviour
+{
+    public Gun gun;
+    public float assistDistance = 0.5f;
+
+    private bool crosshairDelay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        crosshairDelay = gun.crosshairDelay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(Camera.main.ScreenToWorldPoint(gun.crosshair.transform.position), assistDistance, Vector2.zero);
+        if (hits.Any(hit => hit.transform.gameObject.GetComponent<DanceFloorHumanAI>() != null)) 
+        {
+            gun.crosshairDelay = true; //gun.crosshair.transform.position = hits[0].transform.position;
+        }
+        else 
+        {
+            gun.crosshairDelay = crosshairDelay;
+        }
+    }
+}
